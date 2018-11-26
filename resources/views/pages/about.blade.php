100177fb052e66e6b42b9d70ba494b1b8c26e8d5
Ivo Kosir <ivo.kosir93@gmail.com>
2018-11-26 18:01:31 +0100

    Change about text.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,7 @@
 <div class="jumbotron">
     <div class="container">
           <h1 class="display-3">{{ config('app.name') }}</h1>
-          <p>Create online polls with advance voting system so you can look smug.</p>
+          <p>Create online polls with cool voting systems.</p>
     </div>
 </div>
 @endsection
